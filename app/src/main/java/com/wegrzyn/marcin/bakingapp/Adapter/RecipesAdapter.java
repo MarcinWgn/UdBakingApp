d28215d0214e910a2ac67a319e32c6b035e8b5fd
Marcin <wireamg@gmail.com>
2018-04-19 22:51:44 +0200

    add MainActivity RecyclerView

---
+++ app/src/main/java/com/wegrzyn/marcin/bakingapp/Adapter/RecipesAdapter.java
@@ -0,0 +1,75 @@
+package com.wegrzyn.marcin.bakingapp.Adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.wegrzyn.marcin.bakingapp.Model.Recipe;
+import com.wegrzyn.marcin.bakingapp.R;
+
+import java.util.List;
+
+/**
+ * Created by Marcin Węgrzyn on 19.04.2018.
+ * wireamg@gmail.com
+ */
+public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.RecipesViewHolder> {
+
+    private final Context context;
+    private List<Recipe> recipeList;
+    final private ListItemClickListener itemClickListener;
+
+    public RecipesAdapter(Context context, List<Recipe> recipeList, ListItemClickListener itemClickListener ) {
+        this.context = context;
+        this.recipeList = recipeList;
+        this.itemClickListener = itemClickListener;
+    }
+
+    public void setRecipeList(List<Recipe> recipeList) {
+        this.recipeList = recipeList;
+    }
+
+    @NonNull
+    @Override
+    public RecipesViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.recipe_card_item,parent,false);
+
+        return new RecipesViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecipesViewHolder holder, int position) {
+
+        holder.recipeNameTextView.setText(recipeList.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        if (recipeList == null) return 0;
+        else return recipeList.size();
+    }
+    public interface ListItemClickListener {
+        void onListItemClick(int clickItemIndex);
+    }
+    public class RecipesViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        final TextView recipeNameTextView;
+
+        RecipesViewHolder(View itemView) {
+            super(itemView);
+
+            recipeNameTextView = itemView.findViewById(R.id.recipe_name_tv);
+            recipeNameTextView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            itemClickListener.onListItemClick(getAdapterPosition());
+        }
+    }
+}
--- app/src/main/java/com/wegrzyn/marcin/bakingapp/MainActivity.java
+++ app/src/main/java/com/wegrzyn/marcin/bakingapp/MainActivity.java
@@ -2,8 +2,13 @@ package com.wegrzyn.marcin.bakingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
 
+import com.wegrzyn.marcin.bakingapp.Adapter.RecipesAdapter;
 import com.wegrzyn.marcin.bakingapp.Http.HttpOperation;
 import com.wegrzyn.marcin.bakingapp.Http.HttpUtils;
 import com.wegrzyn.marcin.bakingapp.Model.Recipe;
@@ -14,21 +19,40 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RecipesAdapter.ListItemClickListener {
 
     private static final String TAG = MainActivity.class.getSimpleName() ;
+
+    private ProgressBar progressBar;
+    private RecipesAdapter adapter;
+    private List<Recipe> recipeList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        progressBar = findViewById(R.id.progress);
+
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        RecyclerView.LayoutManager mLayoutManager =
+                new GridLayoutManager(this, 1);
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        adapter = new RecipesAdapter(this,recipeList,this);
+        recyclerView.setAdapter(adapter);
+
         getRecipes();
     }
 
 
-
     private void getRecipes(){
+
+        progressBar.setVisibility(View.VISIBLE);
+
         HttpOperation httpOperation = HttpUtils.getHttpService();
         Call<List<Recipe>> call = httpOperation.getRecipes();
 
@@ -37,21 +61,25 @@ public class MainActivity extends AppCompatActivity {
             public void onResponse(Call<List<Recipe>> call, Response<List<Recipe>> response) {
                 Log.d(TAG,"\n"+"onResponse"+"\n");
                 if(response.isSuccessful()){
-                    for (Recipe recipe : response.body()) {
-
-                        Log.d(TAG, "\n"+"id: "+ recipe.getId());
-                        Log.d(TAG, "element: "+ recipe.getName());
-                        Log.d(TAG, "image: "+ recipe.getImage());
-                        Log.d(TAG, "servings: "+ recipe.getServings());
-                    }
+                    recipeList = response.body();
+                    adapter.setRecipeList(recipeList);
+                    adapter.notifyDataSetChanged();
+                    progressBar.setVisibility(View.INVISIBLE);
                 }
 
             }
 
             @Override
             public void onFailure(Call<List<Recipe>> call, Throwable t) {
-                Log.d(TAG," onFailure"+ t.getMessage());
+                Log.d(TAG," onFailure: "+ t.getMessage());
+                progressBar.setVisibility(View.INVISIBLE);
             }
         });
+    }
+
+    @Override
+    public void onListItemClick(int clickItemIndex) {
+
+        Log.d(TAG, String.valueOf(clickItemIndex)+" item cliced");
     }
 }
